765a60473618d35a0bfca69f963e379adf4476c4
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2022-05-10 11:01:54 -0500

    Update content_filtering.py

--- content_filtering.py
+++ content_filtering.py
@@ -2,21 +2,21 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# creating df
+# crear df
 df = pd.read_csv('final.csv')
 
-# notna function maps exisitng elements with true and non exisiting elements to false
-# this operation removes rows mapped to false
+# notna función maps elementos existentes con elementos verdaderos y no existentes a false 
+# esta operación elmina filas mapeadas en false  
 df = df[df['soup'].notna()]
 
-# creating matix / vector
+# crear matríz / vector
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(df['soup'])
 
 # similarity object : classifier
 cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
 
-# resetting index of dataframe
+# resetear índice de dataframe
 df = df.reset_index()
 indices = pd.Series(df.index, index = df['original_title'])
 
